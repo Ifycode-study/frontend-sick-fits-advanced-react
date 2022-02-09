c82cfe5bfb06ac843a37036b228434e2d841da29
Ifycode <marychord@gmail.com>
2022-02-09 23:32:17 +0100

    Learning: Setting up apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 import '../components/styles/nprogress.css';
 //import 'nprogress/nprogress.css';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
+
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -10,12 +13,27 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 // Component refers to the function component in index.js, sell.js etc.
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
     //console.log(Component);
     //console.log(pageProps);
+    //console.log(apollo);
     return (
-        <Page cool='Test prop: from _app.js'>
-            <Component {...pageProps} />
-        </Page>
+        <ApolloProvider client={apollo}>
+            <Page cool='Test prop: from _app.js'>
+                <Component {...pageProps} />
+            </Page>
+        </ApolloProvider>
     );
 }
+
+//Tell appollo to fetch queries in all children Components
+MyApp.getInitialProps = async function({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+    pageProps.query = ctx.query;
+    return { pageProps };
+}
+
+export default withData(MyApp);
